92e2ae91cd4692c31919fb7c3f7860221b8b9376
Lyn Scott <lennord@gmail.com>
2017-08-11 09:13:55 -0700

    Update 3

--- news/newsdb.py
+++ news/newsdb.py
@@ -5,66 +5,65 @@ import psycopg2
 
 
 def connect(database_name="news"):
+    """Connect to the database, create cursor for resuse in multiple f(x)s"""
     try:
         db = psycopg2.connect("dbname={}".format(database_name))
         cursor = db.cursor()
         return db, cursor
     except:
-        print("<error message>")
+        print("Error: Cannot connect to database")
 
 
 def get_top_articles():
     """Return the top articles from the 'database' with the amount of views"""
     db, cursor = connect()
-    top_articles = "select title from articles, log count(path) "
-    "as num "
-    "from articles left join log "
-    "on log.path = concat('/article/', articles.slug) "
-    "and log.status = '200 OK' "
-    "group by title "
-    "order by num desc limit 3; "
+    top_articles = """select articles.title, count(log.path) as num
+    from articles left join log
+          on log.path = concat('/article/', articles.slug)
+          and log.status = '200 OK'
+    group by title
+    order by num desc limit 3;"""
     cursor.execute(top_articles)
-    print("Most popular articles")
+    print("Most popular articles:" '\n')
     for (title, num) in cursor.fetchall():
         print("    {} - {} views".format(title, num))
-        print("-" * 70)
+    print("-" * 70)
     db.close()
 
 
 def get_top_authors():
     """Return top authors from the database with amount of views"""
     db, cursor = connect()
-    top_authors = "select name from authors, log count(path) as num "
-    "from authors, log, articles "
-    "where log.path = concat('/article/', articles.slug) "
-    "and articles.author = authors.id "
-    "and log.status = '200 OK' "
-    "group by authors.name "
-    "order by num desc; "
+    top_authors = """select authors.name, count(log.path) as num
+          from authors, log, articles
+          where log.path = concat('/article/', articles.slug)
+          and articles.author = authors.id
+          and log.status = '200 OK'
+      group by authors.name
+      order by num desc;"""
     cursor.execute(top_authors)
-    print("Most popular authors")
-    for (name, num) in c.fetchall():
+    print("Most popular authors:" '\n')
+    for (name, num) in cursor.fetchall():
         print("    {} - {} views".format(name, num))
-        print("-" * 70)
+    print("-" * 70)
     db.close()
 
 
 def get_errors():
     """View days when HTTP response error exceeds 1%"""
     db, cursor = connect()
-    errors = "select date, round(100 * (errors * 1.0 / total), 1) "
-    "as percentage from "
-    "(select date(time), "
-    "count(case when status = '404 NOT FOUND' "
-    "then 1 else null end) as errors, "
-    "count(*) as total "
-    "from log group by date(time)) as visits "
-    "where round(100* (errors * 1.0 / total),1) > 1.0; "
-    cursor.execute(top_articles)
-    print("Days with more than 1% errors:")
+    errors = """select date, round(100* (errors * 1.0 / total),1) as percentage
+           from (select date(time),
+           count(case when status = '404 NOT FOUND'
+           then 1 else null end) as errors,
+           count(*) as total
+           from log group by date(time)) as visits
+           where round(100* (errors * 1.0 / total),1) > 1.0;"""
+    cursor.execute(errors)
+    print("Days with more than 1% errors:" '\n')
     for (date, percentage) in cursor.fetchall():
-        print("    {} - {} errors".format(date, percentage))
-        print("-" * 70)
+        print("    {:%B %d, %Y} - {}% errors".format(date, percentage))
+    print("-" * 70)
     db.close
 
 if __name__ == "__main__":
